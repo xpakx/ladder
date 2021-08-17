217691d362d5fae3463561184c475f9ee795655a
xpakx <55586470+xpakx@users.noreply.github.com>
2021-08-17 11:38:14 +0200

    test(backend): add tests for getting Project

--- backend/src/test/java/io/github/xpakx/ladder/controller/LabelControllerTest.java
+++ backend/src/test/java/io/github/xpakx/ladder/controller/LabelControllerTest.java
@@ -1,5 +1,9 @@
 package io.github.xpakx.ladder.controller;
 
+import io.github.xpakx.ladder.entity.Project;
+import io.github.xpakx.ladder.entity.UserAccount;
+import io.github.xpakx.ladder.repository.ProjectRepository;
+import io.github.xpakx.ladder.repository.UserAccountRepository;
 import io.github.xpakx.ladder.security.JwtTokenUtil;
 import io.github.xpakx.ladder.service.UserService;
 import org.junit.jupiter.api.AfterEach;
@@ -9,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
+import java.util.HashSet;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
@@ -22,35 +28,81 @@ class LabelControllerTest {
     private int port;
 
     private String baseUrl;
+    private Integer userId;
 
     @Autowired
     JwtTokenUtil jwtTokenUtil;
 
     @Autowired
     UserService userService;
+    @Autowired
+    UserAccountRepository userRepository;
+    @Autowired
+    ProjectRepository projectRepository;
 
     @BeforeEach
     void setUp() {
         baseUrl = "http://localhost".concat(":").concat(port + "");
+        UserAccount user = UserAccount.builder()
+                .username("user1")
+                .password("password")
+                .roles(new HashSet<>())
+                .build();
+        user = userRepository.save(user);
+        this.userId = user.getId();
     }
 
     @AfterEach
     void tearDown() {
+        projectRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     private String tokenFor(String username) {
         return jwtTokenUtil.generateToken(userService.loadUserToLogin(username));
     }
 
+    private Integer addProjectWithParentAndReturnId() {
+        Project parent = Project.builder()
+                .owner(userRepository.getById(userId))
+                .name("Test Parent Project")
+                .build();
+        parent = projectRepository.save(parent);
+        Project project = Project.builder()
+                .owner(userRepository.getById(userId))
+                .name("Test Project")
+                .parent(parent)
+                .build();
+        parent.setChildren(Collections.singletonList(project));
+        return projectRepository.save(project).getId();
+    }
+
 
     @Test
-    void shouldRespondWith401ToGetAllCoursesRequestIfUserUnauthorized() {
+    void shouldRespondWith401ToGetProjectIfUserUnauthorized() {
         given()
                 .log()
                 .uri()
-                .when()
-                .get(baseUrl)
-                .then()
+        .when()
+                .get(baseUrl + "/{userId}/projects/{projectId}", 1, 1)
+        .then()
                 .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWithProjectDetails() {
+        Integer projectId = addProjectWithParentAndReturnId();
+        given()
+                .log()
+                .uri()
+                .auth()
+                .oauth2(tokenFor("user1"))
+        .when()
+                .get(baseUrl + "/{userId}/projects/{projectId}", userId, projectId)
+        .then()
+                .statusCode(OK.value())
+                .body("id", equalTo(projectId))
+                .body("name", equalTo("Test Project"))
+                .body("parent.name", equalTo("Test Parent Project"));
     }
 }
