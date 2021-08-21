cbf3d13d77b45cd849df9ae9b42e81f7339f4fc5
xpakx <55586470+xpakx@users.noreply.github.com>
2021-08-21 23:08:22 +0200

    fix(backend): send proper error when adding project with nonexistent parent

---
+++ backend/src/main/java/io/github/xpakx/ladder/controller/EntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package io.github.xpakx.ladder.controller;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ValidationException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+@ControllerAdvice
+public class EntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleBadRequestException(final ConstraintViolationException exception,
+                                                            final WebRequest request) {
+        Dictionary<String, String> body = new Hashtable<>();
+        body.put("message", exception.getMessage());
+        return handleExceptionInternal(exception, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
