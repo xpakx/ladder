2102513ae634978b588f148a15dd4dd5d7ec6a7d
xpakx <55586470+xpakx@users.noreply.github.com>
2021-09-06 14:21:18 +0200

    fix(frontend): delete test data

--- frontend/src/app/service/tree.service.ts
+++ frontend/src/app/service/tree.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { FullProjectTree } from '../entity/full-project-tree';
 import { LabelDetails } from '../entity/label-details';
 import { ProjectDetails } from '../entity/project-details';
 import { TaskDetails } from '../entity/task-details';
-import { TaskWithChildren } from '../entity/task-with-children';
 import { UserWithData } from '../entity/user-with-data';
 
 @Injectable({
@@ -23,17 +21,8 @@ export class TreeService {
   }
   
   getByDate(date: Date): TaskDetails[] {
-    this.tasks.push({
-      id: 1,
-      title: "Test task",
-      description: "Test task description",
-      due: new Date(),
-      project: {id: 5, name: "Project"},
-      parent: {id: 5}
+    return this.tasks.filter((a) => {
+      a.due.getDate() === date.getDate() && a.due.getMonth() === date.getMonth() && a.due.getFullYear() === date.getFullYear() 
     });
-    return this.tasks;
-    //return this.tasks.filter((a) => {
-    //  a.due.getDate() === date.getDate() && a.due.getMonth() === date.getMonth() && a.due.getFullYear() === date.getFullYear() 
-    //});
   }
 }
