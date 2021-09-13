22e115644f17a6d76822a5cccdf335b73284a35c
xpakx <55586470+xpakx@users.noreply.github.com>
2021-09-13 14:47:42 +0200

    feat(frontend/ui): add forms to edit/add tasks in project view

--- frontend/src/app/component/project/project.component.ts
+++ frontend/src/app/component/project/project.component.ts
@@ -16,6 +16,8 @@ export class ProjectComponent implements OnInit {
   tasks: TaskDetails[] = [];
   todayDate: Date | undefined;
   project: ProjectTreeElem | undefined;
+  showAddTaskForm: boolean = false;
+  showEditTaskFormById: number | undefined;
 
   constructor(private router: Router, private route: ActivatedRoute, 
     private tree: TreeService) { }
@@ -29,5 +31,23 @@ export class ProjectComponent implements OnInit {
   loadProject(id: number) {
     this.project = this.tree.getProjectById(id);
     this.tasks = this.tree.getTasksByProject(id);
+  }
+
+  openAddTaskForm() {
+    this.closeEditTaskForm();
+    this.showAddTaskForm = true;
+  }
+
+  closeAddTaskForm() {
+    this.showAddTaskForm = false;
+  }
+
+  openEditTaskForm(id: number) {
+    this.closeAddTaskForm();
+    this.showEditTaskFormById = id;
+  }
+
+  closeEditTaskForm() {
+    this.showEditTaskFormById = undefined;
   }
 }
