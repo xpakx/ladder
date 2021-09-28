07c03eb25834f92180c760366511e40f66fcb953
xpakx <55586470+xpakx@users.noreply.github.com>
2021-09-28 16:27:01 +0200

    feat(frontend): add option to choose no date for task

--- frontend/src/app/component/task-form/task-form.component.ts
+++ frontend/src/app/component/task-form/task-form.component.ts
@@ -41,7 +41,7 @@ export class TaskFormComponent implements OnInit {
       this.tomorrow.setDate(dayOfTheMonth + 1);
       this.weekend = new Date();
       this.weekend.setDate(dayOfTheMonth - dayOfTheWeek + 6);
-      this.nextWeek = new Date();
+      this.nextWeek = new Date(this.weekend);
       this.nextWeek.setDate(this.weekend.getDate() + 2);
     }
 
@@ -78,13 +78,41 @@ export class TaskFormComponent implements OnInit {
   }
 
   dateWithinWeek(date: Date): boolean {
-    let dateToCompare: Date = new Date(this.today);
+    let dateToCompare: Date = new Date();
     dateToCompare.setDate(dateToCompare.getDate() + 9);
     dateToCompare.setHours(0);
     dateToCompare.setMinutes(0);
     dateToCompare.setSeconds(0);
     dateToCompare.setMilliseconds(0);
+    return date < dateToCompare && !this.isOverdue(date);
+  }
+
+  isOverdue(date: Date): boolean {
+    let dateToCompare: Date = new Date();
+    dateToCompare.setHours(0);
+    dateToCompare.setMinutes(0);
+    dateToCompare.setSeconds(0);
     return date < dateToCompare;
+  }
+
+  sameDay(date1: Date, date2: Date): boolean {
+    return date1.getFullYear() == date2.getFullYear() && date1.getDate() == date2.getDate() && date1.getMonth() == date2.getMonth();
+  }
+
+  isToday(date: Date): boolean {
+    let today = new Date();
+    return this.sameDay(today, date);
+  }
+
+  isTomorrow(date: Date): boolean {
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return this.sameDay(tomorrow, date);
+  }
+
+  thisYear(date: Date): boolean {
+    let today = new Date();
+    return today.getFullYear() == date.getFullYear();
   }
 
   openSelectDateMenu() {
