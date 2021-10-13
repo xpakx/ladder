f522dc538d7218810d9d0c71bc9066f9d2579d64
xpakx <55586470+xpakx@users.noreply.github.com>
2021-10-13 22:11:57 +0200

    chore: add microservice for notifications

---
+++ notification-service/src/test/java/io/github/xpakx/laddernotify/LadderNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.laddernotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadderNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
