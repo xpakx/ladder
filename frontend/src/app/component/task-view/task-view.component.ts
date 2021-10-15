162eefa224b71fa906edb7ebf2a95820b4ba8cec
xpakx <55586470+xpakx@users.noreply.github.com>
2021-10-15 18:30:28 +0200

    feat(frontend): add option to edit task in task view

--- frontend/src/app/component/task-view/task-view.component.ts
+++ frontend/src/app/component/task-view/task-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TaskTreeElem } from 'src/app/entity/task-tree-elem';
+import { AddEvent } from 'src/app/entity/utils/add-event';
 import { TaskTreeService } from 'src/app/service/task-tree.service';
 
 @Component({
@@ -10,15 +11,22 @@ import { TaskTreeService } from 'src/app/service/task-tree.service';
 export class TaskViewComponent implements OnInit {
   @Input("task") parent?: TaskTreeElem;
   @Output() closeEvent = new EventEmitter<boolean>();
+  edit: boolean = false;
+  parentData!: AddEvent<TaskTreeElem>;
 
   constructor(private taskTree: TaskTreeService) { }
 
-  
-
   ngOnInit(): void {
+    if(parent) {
+      this.parentData = new AddEvent<TaskTreeElem>(this.parent);
+    }
   }
 
   closeModal() {
     this.closeEvent.emit(true);
+  }
+
+  closeEditTaskForm() {
+    this.edit = false;
   }
 }
