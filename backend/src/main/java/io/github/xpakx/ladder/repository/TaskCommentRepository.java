efdb2edc975446ba77f584d6f4ad3e9d969dd131
xpakx <55586470+xpakx@users.noreply.github.com>
2021-10-18 15:37:58 +0200

    feat(backend): add option to add and delete comments for task

--- backend/src/main/java/io/github/xpakx/ladder/repository/TaskCommentRepository.java
+++ backend/src/main/java/io/github/xpakx/ladder/repository/TaskCommentRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskCommentRepository extends JpaRepository<TaskComment, Integer> {
     Page<TaskCommentDetails> findByTaskId(Integer taskId, Pageable page);
+
+    Optional<TaskComment> getByIdAndUserId(Integer id, Integer userId);
 }
--- backend/src/main/java/io/github/xpakx/ladder/service/TaskCommentService.java
+++ backend/src/main/java/io/github/xpakx/ladder/service/TaskCommentService.java
@@ -1,20 +1,30 @@
 package io.github.xpakx.ladder.service;
 
+import io.github.xpakx.ladder.entity.Project;
+import io.github.xpakx.ladder.entity.Task;
 import io.github.xpakx.ladder.entity.TaskComment;
+import io.github.xpakx.ladder.entity.dto.AddCommentRequest;
+import io.github.xpakx.ladder.entity.dto.AddTaskRequest;
 import io.github.xpakx.ladder.entity.dto.TaskCommentDetails;
+import io.github.xpakx.ladder.error.NotFoundException;
 import io.github.xpakx.ladder.repository.TaskCommentRepository;
+import io.github.xpakx.ladder.repository.TaskRepository;
+import io.github.xpakx.ladder.repository.UserAccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TaskCommentService {
     private final TaskCommentRepository commentRepository;
+    private final TaskRepository taskRepository;
+    private final UserAccountRepository userRepository;
 
     public Page<TaskCommentDetails> getAllForTask(Integer taskId, Integer page) {
         return commentRepository.findByTaskId(
@@ -23,5 +33,24 @@ public class TaskCommentService {
         );
     }
 
+    public TaskComment addComment(AddCommentRequest request, Integer taskId, Integer userId) {
+        TaskComment commentToAdd = buildCommentToAddFromRequest(request, taskId, userId);
+        return commentRepository.save(commentToAdd);
+    }
+
+    public TaskComment buildCommentToAddFromRequest(AddCommentRequest request, Integer taskId, Integer userId) {
+        return TaskComment.builder()
+                .owner(userRepository.getById(userId))
+                .task(taskRepository.getById(taskId))
+                .createdAt(LocalDateTime.now())
+                .content(request.getContent())
+                .build();
+    }
+
+    public void deleteComment(Integer commentId, Integer userId) {
+        TaskComment comment = commentRepository.getByIdAndUserId(commentId, userId)
+                .orElseThrow(() -> new NotFoundException("No such comment!"));
+        commentRepository.delete(comment);
+    }
 
 }
