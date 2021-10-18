bc60beac238b96b1415c02fde05e2eda15d00b37
xpakx <55586470+xpakx@users.noreply.github.com>
2021-10-18 14:47:03 +0200

    feat(frontend): add option to change date in task view

--- frontend/src/app/component/task-view/task-view.component.ts
+++ frontend/src/app/component/task-view/task-view.component.ts
@@ -45,4 +45,67 @@ export class TaskViewComponent implements OnInit {
     );
     }
   }
+
+  dateWithinWeek(date: Date): boolean {
+    let dateToCompare: Date = new Date();
+    dateToCompare.setDate(dateToCompare.getDate() + 9);
+    dateToCompare.setHours(0);
+    dateToCompare.setMinutes(0);
+    dateToCompare.setSeconds(0);
+    dateToCompare.setMilliseconds(0);
+    return date < dateToCompare && !this.isOverdue(date);
+  }
+
+  isOverdue(date: Date): boolean {
+    let dateToCompare: Date = new Date();
+    dateToCompare.setHours(0);
+    dateToCompare.setMinutes(0);
+    dateToCompare.setSeconds(0);
+    return date < dateToCompare;
+  }
+
+  sameDay(date1: Date, date2: Date): boolean {
+    return date1.getFullYear() == date2.getFullYear() && date1.getDate() == date2.getDate() && date1.getMonth() == date2.getMonth();
+  }
+
+  isToday(date: Date): boolean {
+    let today = new Date();
+    return this.sameDay(today, date);
+  }
+
+  isTomorrow(date: Date): boolean {
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return this.sameDay(tomorrow, date);
+  }
+
+  thisYear(date: Date): boolean {
+    let today = new Date();
+    return today.getFullYear() == date.getFullYear();
+  }
+
+  showSelectDateModal:boolean = false;
+
+  openSelectDateModal() {
+    this.showSelectDateModal = true;
+  }
+
+  closeSelectDateModal(date: Date | undefined) {
+    this.showSelectDateModal = false;
+    if(this.parent) {
+      this.taskService.updateTaskDueDate({date: date}, this.parent.id).subscribe(
+          (response: Task) => {
+          this.taskTree.updateTaskDate(response);
+        },
+        (error: HttpErrorResponse) => {
+        
+        }
+      );
+    }
+    this.showSelectDateModal = false;
+  }
+  
+  cancelDateSelection() {
+    this.showSelectDateModal = false;
+  }
 }
