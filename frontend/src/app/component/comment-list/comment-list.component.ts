15f253bec1be13bce0df766ee81214c1939873a9
xpakx <55586470+xpakx@users.noreply.github.com>
2021-10-21 13:52:42 +0200

    feat: add option to add comment for task

--- frontend/src/app/component/comment-list/comment-list.component.ts
+++ frontend/src/app/component/comment-list/comment-list.component.ts
@@ -4,6 +4,8 @@ import { TaskTreeElem } from 'src/app/entity/task-tree-elem';
 import { Page } from 'src/app/entity/page';
 import { CommentService } from '../../service/comment.service';
 import { TaskCommentDetails } from '../../entity/task-comment-details';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TaskComment } from 'src/app/entity/task-comment';
 
 @Component({
   selector: 'app-comment-list',
@@ -13,10 +15,15 @@ import { TaskCommentDetails } from '../../entity/task-comment-details';
 export class CommentListComponent implements OnInit {
   @Input('task') task?: TaskTreeElem;
   comments:TaskCommentDetails[] = [];
+  commentForm: FormGroup | undefined;
 
-  constructor(private commentService: CommentService) {}
+  constructor(private commentService: CommentService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.commentForm = this.fb.group({
+      content: ['', Validators.required]
+    });
+
     if(this.task) {
       this.commentService.getCommentsForTask(this.task.id).subscribe(
         (response: Page<TaskCommentDetails>) => {
@@ -29,7 +36,22 @@ export class CommentListComponent implements OnInit {
   }
 
   sendComment() {
-   
+    if(this.task && this.commentForm) {
+      this.commentService.addComment({content: this.commentForm.controls.content.value}, 
+        this.task.id).subscribe(
+        (response: TaskComment) => {
+          this.comments.push(
+              {
+                id: response.id,
+                content: response.content,
+                createdAt: response.createdAt,
+              }
+            );
+      },
+      (error: HttpErrorResponse) => {
+      
+      });
+    }
   }
 
 }
