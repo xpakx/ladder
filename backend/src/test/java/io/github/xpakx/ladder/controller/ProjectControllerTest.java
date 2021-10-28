d9f9e668f4e40faab224a480a4acb24253007217
xpakx <55586470+xpakx@users.noreply.github.com>
2021-10-28 17:50:46 +0200

    test(backend): add tests for moving projects as first

--- backend/src/test/java/io/github/xpakx/ladder/controller/ProjectControllerTest.java
+++ backend/src/test/java/io/github/xpakx/ladder/controller/ProjectControllerTest.java
@@ -1242,8 +1242,50 @@ class ProjectControllerTest {
                 .statusCode(OK.value());
 
         List<Project> projects = projectRepository.findAll();
-        projects.forEach((a) -> System.out.println(a.getName() + " " + a.getGeneralOrder()));
         assertThat(projects, hasSize(4));
+        assertThat(projects, hasItem(allOf(
+                hasProperty("name", is("Project 1")),
+                hasProperty("generalOrder", is(2))
+        )));
+        assertThat(projects, hasItem(allOf(
+                hasProperty("name", is("Project 2")),
+                hasProperty("generalOrder", is(3))
+        )));
+        assertThat(projects, hasItem(allOf(
+                hasProperty("name", is("Project 3")),
+                hasProperty("generalOrder", is(1))
+        )));
+    }
+
+    @Test
+    void shouldRespondWith401ToMoveProjectAsFirstIfUserUnauthorized() {
+        given()
+                .log()
+                .uri()
+        .when()
+                .put(baseUrl + "/{userId}/projects/{projectId}/move/asFirst", 1, 1)
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldMoveProjectAsFirst() {
+        List<Integer> ids = add3ProjectsInOrderAndReturnListOfIds();
+
+        Integer projectId = ids.get(2);
+        given()
+                .log()
+                .uri()
+                .auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+        .when()
+                .put(baseUrl + "/{userId}/projects/{projectId}/move/asFirst", userId, projectId)
+        .then()
+                .statusCode(OK.value());
+
+        List<Project> projects = projectRepository.findAll();
+        assertThat(projects, hasSize(3));
         assertThat(projects, hasItem(allOf(
                 hasProperty("name", is("Project 1")),
                 hasProperty("generalOrder", is(2))
