cec5a753cbaf6adea0f341c7f5e9a1c9656635bb
xpakx <55586470+xpakx@users.noreply.github.com>
2021-11-03 23:23:42 +0100

    feat(frontend): add service method for delete habit

--- frontend/src/app/service/habit.service.ts
+++ frontend/src/app/service/habit.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,5 +15,8 @@ export class HabitService {
     return localStorage.getItem("user_id");
   }
 
-  
+  public deleteHabit(habitId: number):  Observable<any> {
+    let userId  = this.getUserId();
+    return this.http.delete<any>(`${this.apiServerUrl}/${userId}/habits/${habitId}`);
+  }
 }
