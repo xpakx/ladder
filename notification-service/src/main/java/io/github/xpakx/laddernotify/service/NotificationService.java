a45a2fc68b3e011c010998178be18ffcd9964822
xpakx <55586470+xpakx@users.noreply.github.com>
2022-03-03 17:37:50 +0100

    fix(backend): prevent sending notifications to wrong users

--- notification-service/src/main/java/io/github/xpakx/laddernotify/service/NotificationService.java
+++ notification-service/src/main/java/io/github/xpakx/laddernotify/service/NotificationService.java
@@ -2,6 +2,7 @@ package io.github.xpakx.laddernotify.service;
 
 import io.github.xpakx.laddernotify.entity.Notification;
 import io.github.xpakx.laddernotify.entity.NotificationRequest;
+import io.github.xpakx.laddernotify.utils.CustomEmitter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.http.MediaType;
@@ -9,13 +10,14 @@ import org.springframework.http.MediaType;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class NotificationService {
-    List<SseEmitter> emitters = new ArrayList<>();
+    List<CustomEmitter> emitters = new ArrayList<>();
 
     public SseEmitter subscribe(Integer userId) {
-        SseEmitter emitter = new SseEmitter(24 * 60 * 60 * 1000L);
+        CustomEmitter emitter = new CustomEmitter(userId, 24 * 60 * 60 * 1000L);
         emitter.onCompletion(() -> {
 			emitters.remove(emitter);
 			});
@@ -25,24 +27,25 @@ public class NotificationService {
     }
 
     public void pushNotification(NotificationRequest request) {
-        List<SseEmitter> deadEmitters = new ArrayList<>();
+        List<CustomEmitter> deadEmitters = new ArrayList<>();
 
         Notification notification = new Notification(request.getTime(), request.getType(), request.getId());
 
-        emitters.forEach(emitter -> {
-            try {
-                sendNotification(request.getUserId(), notification, emitter);
+        emitters.stream()
+                .filter((a) -> Objects.equals(a.getUserId(), request.getUserId()))
+                .forEach(emitter -> {
+                    try {
+                        sendNotification(notification, emitter);
 
-            } catch (IOException e) {
-                deadEmitters.add(emitter);
-            }
+                    } catch (IOException e) {
+                        deadEmitters.add(emitter);
+                    }
         });
 
         emitters.removeAll(deadEmitters);
     }
 
-    private void sendNotification(Integer username, Notification payload, SseEmitter emitter) throws IOException {
+    private void sendNotification(Notification payload, CustomEmitter emitter) throws IOException {
         emitter.send(payload, MediaType.APPLICATION_JSON);
-
     }
 }
