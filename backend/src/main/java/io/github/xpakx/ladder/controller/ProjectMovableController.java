3a51346713d95e99155f2add30d72a8300dc3416
xpakx <55586470+xpakx@users.noreply.github.com>
2022-03-24 14:40:13 +0100

    fix(backend): add request mapping

--- backend/src/main/java/io/github/xpakx/ladder/controller/ProjectMovableController.java
+++ backend/src/main/java/io/github/xpakx/ladder/controller/ProjectMovableController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/{userId}/projects")
 @AllArgsConstructor
 public class ProjectMovableController {
     private final ProjectMovableService projectService;
