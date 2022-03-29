13baf99625dbdd94a1d02a9a7ad1ed3b9131c947
xpakx <55586470+xpakx@users.noreply.github.com>
2022-03-29 15:43:35 +0200

    chore(backend): add class for testing ProjectCollaborationService

---
+++ backend/src/test/java/io/github/xpakx/ladder/controller/ProjectCollaborationControllerTest.java
@@ -0,0 +1,59 @@
+package io.github.xpakx.ladder.controller;
+
+import io.github.xpakx.ladder.entity.UserAccount;
+import io.github.xpakx.ladder.repository.ProjectRepository;
+import io.github.xpakx.ladder.repository.TaskRepository;
+import io.github.xpakx.ladder.repository.UserAccountRepository;
+import io.github.xpakx.ladder.security.JwtTokenUtil;
+import io.github.xpakx.ladder.service.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.HashSet;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProjectCollaborationControllerTest {
+    @LocalServerPort
+    private int port;
+
+    private String baseUrl;
+    private Integer userId;
+
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserAccountRepository userRepository;
+    @Autowired
+    ProjectRepository projectRepository;
+    @Autowired
+    TaskRepository taskRepository;
+
+    @BeforeEach
+    void setUp() {
+        baseUrl = "http://localhost".concat(":").concat(port + "");
+        UserAccount user = UserAccount.builder()
+                .username("user1")
+                .password("password")
+                .roles(new HashSet<>())
+                .build();
+        user = userRepository.save(user);
+        this.userId = user.getId();
+    }
+
+    @AfterEach
+    void tearDown() {
+        taskRepository.deleteAll();
+        projectRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    private String tokenFor(String username) {
+        return jwtTokenUtil.generateToken(userService.loadUserToLogin(username));
+    }
+
+}
