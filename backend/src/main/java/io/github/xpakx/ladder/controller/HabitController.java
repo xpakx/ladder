066871b19f8e6187b00838ad53bb0a396fc65ef7
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-05 14:44:14 +0200

    refactor(backend): delete unused imports

--- backend/src/main/java/io/github/xpakx/ladder/controller/HabitController.java
+++ backend/src/main/java/io/github/xpakx/ladder/controller/HabitController.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/{userId}")
 @AllArgsConstructor
--- backend/src/main/java/io/github/xpakx/ladder/controller/ProjectController.java
+++ backend/src/main/java/io/github/xpakx/ladder/controller/ProjectController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/{userId}/projects")
 @AllArgsConstructor
