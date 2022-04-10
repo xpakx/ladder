ae63d6c8bc0f641e72c98eb35992a0b82abc84be
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-10 12:48:39 +0200

    refactor(backend): extract class for tree operation of filters

--- backend/src/main/java/io/github/xpakx/ladder/controller/FilterController.java
+++ backend/src/main/java/io/github/xpakx/ladder/controller/FilterController.java
@@ -41,31 +41,4 @@ public class FilterController {
     public ResponseEntity<Filter> updateLabelFav(@RequestBody BooleanRequest request, @PathVariable Integer filterId, @PathVariable Integer userId) {
         return  new ResponseEntity<>(filterService.updateFilterFav(request, filterId, userId), HttpStatus.OK);
     }
-
-    @PreAuthorize("#userId.toString() == authentication.principal.username")
-    @PutMapping("/{filterId}/move/asFirst")
-    public ResponseEntity<Filter> moveFilterAsFirst(@PathVariable Integer userId, @PathVariable Integer filterId) {
-        return  new ResponseEntity<>(filterService.moveFilterAsFirst(userId, filterId), HttpStatus.OK);
-    }
-
-    @PreAuthorize("#userId.toString() == authentication.principal.username")
-    @PostMapping("/{filterId}/after")
-    public ResponseEntity<Filter> addFilterAfter(@RequestBody FilterRequest request, @PathVariable Integer userId,
-                                               @PathVariable Integer filterId) {
-        return  new ResponseEntity<>(filterService.addFilterAfter(request, userId, filterId), HttpStatus.CREATED);
-    }
-
-    @PreAuthorize("#userId.toString() == authentication.principal.username")
-    @PostMapping("/{filterId}/before")
-    public ResponseEntity<Filter> addFilterBefore(@RequestBody FilterRequest request, @PathVariable Integer userId,
-                                                @PathVariable Integer filterId) {
-        return  new ResponseEntity<>(filterService.addFilterBefore(request, userId, filterId), HttpStatus.CREATED);
-    }
-
-    @PreAuthorize("#userId.toString() == authentication.principal.username")
-    @PutMapping("/{filterId}/move/after")
-    public ResponseEntity<Filter> moveFilterAfter(@RequestBody IdRequest request, @PathVariable Integer userId,
-                                                @PathVariable Integer filterId) {
-        return new ResponseEntity<>(filterService.moveFilterAfter(request, userId, filterId), HttpStatus.OK);
-    }
 }
