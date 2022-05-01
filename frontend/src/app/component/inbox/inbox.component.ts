dff0336a6208fe36122779bdc9cbbadf1a9bbd69
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-01 14:01:13 +0200

    refactor(frontend): delete unused imports

--- frontend/src/app/component/inbox/inbox.component.ts
+++ frontend/src/app/component/inbox/inbox.component.ts
@@ -1,12 +1,7 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { LabelDetails } from 'src/app/entity/label-details';
-import { ProjectTreeElem } from 'src/app/entity/project-tree-elem';
-import { Task } from 'src/app/entity/task';
 import { TaskTreeElem } from 'src/app/entity/task-tree-elem';
 import { AddEvent } from 'src/app/entity/utils/add-event';
-import { DeleteService } from 'src/app/service/delete.service';
 import { RedirectionService } from 'src/app/service/redirection.service';
 import { TaskTreeService } from 'src/app/service/task-tree.service';
 import { TaskService } from 'src/app/service/task.service';
@@ -29,7 +24,6 @@ implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, 
     private tree: TreeService, private taskService: TaskService,
     private taskTreeService: TaskTreeService,
-    private renderer: Renderer2, private deleteService: DeleteService,
     private redirService: RedirectionService) {
     super(taskTreeService, taskService);
   }
